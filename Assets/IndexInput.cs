6361d956a40a76fbfe47671aa01c20e489248271
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2022-03-04 14:42:05 -0400

    [ADDED] LeftHand Model

---
+++ Assets/IndexInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class IndexInput : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 ThumbstickAction = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thumbstick();
+    }
+
+    private void Thumbstick()
+    {
+        if (ThumbstickAction.axis == Vector2.zero)
+            return;
+
+        Debug.Log("Thumbstick: " + ThumbstickAction.axis);
+    }
+}
